2f2638284b18f52da396abb235f94decdd5f0241
Raphael LUCIANO <raphael.luciano@igtp4-mac13.local>
2019-03-25 18:31:41 +0100

    Initial Commit

---
+++ MoneyCount/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyCount
+//
+//  Created by Raphael LUCIANO on 25/03/2019.
+//  Copyright © 2019 Raphael LUCIANO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
